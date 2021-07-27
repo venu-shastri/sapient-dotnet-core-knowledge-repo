cb7b5f002a15d0cd7d535b52aab401ad9c3eb4b8
Venugopal  K (C) <vgk@tritechsamvit.com>
2021-07-27 08:55:09 +0530

    Update WebApi

---
+++ CalulcatorApiLib/CalulcatorApiLib/Controllers/CalculatorController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CalulcatorApiLib.Controllers
+{
+    //URL - http://origin/api/Calculator
+    //Action : GET
+
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculatorController : ControllerBase
+    {
+        [HttpGet]
+        public string GetResult()
+        {
+            return "Sum of .....";
+        }
+        [HttpGet]
+        public string GetNextResult()
+        {
+            return "Next Result...";
+        }
+        [HttpPost]
+        public int Add(int x,int y)
+        {
+            return x + y;
+        }
+        [HttpPost]
+        public int Sub(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
